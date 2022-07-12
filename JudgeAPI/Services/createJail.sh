39c9e9c5c11765192cef308f0120ddb3e1804db3
olix3001 <46107095+olix3001@users.noreply.github.com>
2022-07-12 03:33:07 +0200

    Change createJail.sh to work with TrainAlgs

--- JudgeAPI/Services/createJail.sh
+++ JudgeAPI/Services/createJail.sh
@@ -5,8 +5,6 @@ mkdir jail-submission-$1
 cd jail-submission-$1
 mkdir -p bin lib64/x86_64-linux-gnu lib/x86_64-linux-gnu
 
-mv ../$1-files/* ./
-
 #? execute jail command
 #sudo unshare -n -m "$2"
 
