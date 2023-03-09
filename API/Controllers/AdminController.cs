c7795787e14e33285705939c1280a25f54310d3e
InfoTCube <infocube.yout@gmail.com>
2023-03-09 19:52:57 +0100

    feat: Admin controller

---
+++ API/Controllers/AdminController.cs
@@ -0,0 +1,41 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using API.Entities;
+using API.Interfaces;
+using AutoMapper;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers
+{
+    public class AdminController : BaseApiController
+    {
+        private readonly IUnitOfWork _unitOfWork;
+        private readonly IMapper _mapper;
+        private readonly UserManager<AppUser> _userManager;
+        public AdminController(IUnitOfWork unitOfWork, IMapper mapper, UserManager<AppUser> userManager)
+        {
+            _userManager = userManager;
+            _mapper = mapper;
+            _unitOfWork = unitOfWork;
+        }
+
+        [Authorize(Policy = "RequireAdminRole")]
+        [HttpPut("assignModerator/{username}")]
+        public async Task<ActionResult> AssignModerator(string username)
+        {
+            AppUser user = await _unitOfWork.UserRepository.GetUserByUsernameAsync(username);
+            if(user is null) return NotFound("This user doesn't exist!");
+            if((await _userManager.GetRolesAsync(user)).Any(role => role == "Moderator")) return BadRequest("User is already a moderator");
+
+            var results = await _userManager.AddToRoleAsync(user, "Moderator");
+
+            if (results.Succeeded) return NoContent();
+
+            return BadRequest("Failed to assign user as a moderator");
+        }
+    }
+}
